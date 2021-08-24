3c335df88505831a2673ff3816af1a5f8b34f40e
bennour-med <bennour.ma@gmail.com>
2021-08-24 16:11:52 +0200

    inscription et connexion

--- src/main/java/com/tww/aidecovid/repository/MemberRepository.java
+++ src/main/java/com/tww/aidecovid/repository/MemberRepository.java
@@ -1,13 +1,13 @@
 package com.tww.aidecovid.repository;
 
 import com.tww.aidecovid.model.Member;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByLogin(String login);
 }
